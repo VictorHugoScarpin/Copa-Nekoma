1f5917b987e3aa512e7f4f103166218216fba112
Scarpin_Xavier <vhsxtrabalho@gmail.com>
2026-06-08 01:42:15 -0300

    Update sync-data.js
    Teste API

--- scripts/sync-data.js
+++ scripts/sync-data.js
@@ -5,7 +5,7 @@ const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY
 const FOOTBALL_API_KEY = process.env.FOOTBALL_API_KEY
 
 const LEAGUE_ID = 1
-const SEASON = 2026
+const SEASON = 2022
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY)
 
